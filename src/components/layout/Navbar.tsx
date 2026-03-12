68e19c233a3f877f25b3d32b13d491c21f5bf812
Robert (Agency OS) <228250850+Klerno-Labs@users.noreply.github.com>
2026-03-12 18:04:23 +0000

    fix: fix ServiceCard + add Button import to Navbar [agency-os bot]

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -1,5 +1,6 @@
 "use client"
 
+import { Button } from "@/components/ui/button";
 import React, { useState, useEffect } from 'react'
 import Link from 'next/link'
 import { usePathname } from 'next/navigation'
